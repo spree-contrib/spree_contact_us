2fe76a672d82e8df6d21f9e88ad07331d83dd8a8
Jeff Dutil <jdutil21@gmail.com>
2012-07-11 11:56:48 -0400

    Update Changelog and set preference to nil by default.

--- lib/spree/contact_us_configuration.rb
+++ lib/spree/contact_us_configuration.rb
@@ -1,3 +1,3 @@
 class Spree::ContactUsConfiguration < Spree::Preferences::Configuration
-  preference :contact_tracking_message, :string, :default => ''
+  preference :contact_tracking_message, :string, :default => nil
 end
