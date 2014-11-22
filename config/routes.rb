6b6273dae38153eb289e6cb5ed459b1997bcbe72
Gregorio Setti <gregorio.setti@gmail.com>
2014-11-22 14:24:53 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-#Spree::Core::Engine.add_routes do
-#  resources :contacts,
-#    :controller => 'contact_us/contacts',
-#    :only       => [:new, :create]
-#  get 'contact-us' => 'contact_us/contacts#new', :as => :contact_us
-#end
+Spree::Core::Engine.append_routes do
+  resources :contacts, :controller => 'contact_us/contacts', :only => [:new, :create]
+  get 'contact-us' => 'contact_us/contacts#new', :as => :contact_us
+end
