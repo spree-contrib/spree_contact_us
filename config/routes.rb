8e951e15fe2bb1aa00bca45c766b0d31da718bce
Gregorio Setti <gregorio.setti@gmail.com>
2014-11-22 14:02:15 +0100

    Update draw_routes
    Update dependencies
    
    fixes #40

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-Spree::Core::Engine.routes.draw do
-  resources :contacts,
-    :controller => 'contact_us/contacts',
-    :only       => [:new, :create]
+Spree::Core::Engine.add_routes do
+  resources :contacts, :controller => 'contact_us/contacts', :only => [:new, :create]
   get 'contact-us' => 'contact_us/contacts#new', :as => :contact_us
 end
