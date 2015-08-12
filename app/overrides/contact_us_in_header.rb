4089e70ef39e0a5f7e48fc505abec2259e0afbfc
Jacob Spizziri <jacob.spizziri@gmail.com>
2015-08-12 09:12:36 -0500

    Fix override positioning and bootstrap classes

--- app/overrides/contact_us_in_header.rb
+++ app/overrides/contact_us_in_header.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
                      :name => "contact_us_in_header",
-                     :insert_bottom => "#main-nav-bar",
-                     :text => "<li class='<%= (request.fullpath.gsub('//','/') == '/contact-us') ? 'current' : 'not'%>'><%= link_to Spree.t(:contact_us_title), '/contact-us'  %></li>",
+                     :insert_bottom => "#main-nav-bar .nav.navbar-nav:first-child",
+                     :text => "<li class='<%= (request.fullpath.gsub('//','/') == '/contact-us') ? 'active' : ''%>'><%= link_to Spree.t(:contact_us_title), '/contact-us'  %></li>",
                      :original => '442eefbb91b46a8773ce1de620f8f0a88a66caf1')
